fec8776873cff510580458e4ce985290007e04d0
Jordan Emory <jemory526@gmail.com>
2020-04-16 19:37:54 -0700

    Can retrieve data from api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { users: [] };
+  }
+  componentDidMount() {
+    axios.get(`https://localhost:44303/api/users/`)
+      .then(res => {
+        const users = res.data;
+        this.setState({ users });
+      })
+  }
+  render() {
+    return (
+      <div>
+        <ul className="userList">
+          {this.state.users.map(
+            user => <li>
+              {user.firstName + " " + user.lastName}
+              </li>)
+          }
+        </ul>
+      </div>
+    )
+  }
+}
 export default App;
